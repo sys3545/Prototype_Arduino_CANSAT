22639d196d6b853edc8e41232d98b749070ce041
Sim <61923882+sys3545@users.noreply.github.com>
2020-08-08 20:35:07 +0900

    Bluetooth Serial

--- sketch/Cansatellite/Cansatellite.ino
+++ sketch/Cansatellite/Cansatellite.ino
@@ -1,25 +1,44 @@
+#include <stdio.h>
+#include <string.h>
 #include <SoftwareSerial.h>
+#define LED 8
+
+SoftwareSerial BTSerial(2, 3);   //bluetooth module Tx:Digital 2 Rx:Digital 3
 
 char inStr[6]="";
 int i=0;
 
+void myFlush(){
+  //  버퍼에 있는 데이터를 다 지운다.
+  while(Serial.available()>0)
+    Serial.read();
+}
+
 void setup() {
+  pinMode(LED, OUTPUT);
   Serial.begin(9600);
+  BTSerial.begin(9600);
+  fflush(stdin);
+  Serial.println("ATcommand");  //ATcommand Start
 }
 
 void loop() {
-  if ( Serial.available() ) {
-    char msg = Serial.read();
-    if(msg == '$'){
-      i = 0;
+  if(BTSerial.available()){
+    char msg = BTSerial.read();
+    if(msg=='$'){
+      i=0;
     }
     else{
       inStr[i++]=msg;
     }
-    
-    if(i==4){
-      Serial.write(inStr);
+
+    if(i==5){
+      if(strcmp(inStr,"tuon")==0)
+        digitalWrite(LED, 1);
+      else
+        digitalWrite(LED, 0);
+
+      i=0;
     }
-    
   }
 }
