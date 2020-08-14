65f0c1a714b2abbd6f19dcfa89796b7aeb920e8c
Sim <61923882+sys3545@users.noreply.github.com>
2020-08-14 21:47:58 +0900

    added servo control

--- sketch/Cansatellite/Cansatellite.ino
+++ sketch/Cansatellite/Cansatellite.ino
@@ -1,24 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 #include <SoftwareSerial.h>
-#define LED 8
+#include <Servo.h>
+#define LED 14
 
-SoftwareSerial BTSerial(2, 3);   //bluetooth module Tx:Digital 2 Rx:Digital 3
+SoftwareSerial BTSerial(15, 16);   //bluetooth module Tx:Digital 15 Rx:Digital 16
+Servo servo;
 
 char inStr[6]="";
 int i=0;
-
-void myFlush(){
-  //  버퍼에 있는 데이터를 다 지운다.
-  while(Serial.available()>0)
-    Serial.read();
-}
+int mode=0;
 
 void setup() {
   pinMode(LED, OUTPUT);
+  servo.attach(9);
   Serial.begin(9600);
   BTSerial.begin(9600);
   fflush(stdin);
+  servo.write(0);
   Serial.println("ATcommand");  //ATcommand Start
 }
 
@@ -33,12 +32,23 @@ void loop() {
     }
 
     if(i==5){
-      if(strcmp(inStr,"tuon")==0)
-        digitalWrite(LED, 1);
-      else
-        digitalWrite(LED, 0);
+      if(strcmp(inStr,"tuon")==0){
+        mode = 1;
+        digitalWrite(LED, mode);
+      }
+      else if(strcmp(inStr,"tuof")==0){
+        mode = 0;
+        digitalWrite(LED, mode);
+      }
+      else if(strcmp(inStr,"seon")==0){
+        servo.write(60);
+      }
+      else{
+        servo.write(0);
+      }
 
       i=0;
     }
   }
+  //BTSerial.write(mode);
 }
